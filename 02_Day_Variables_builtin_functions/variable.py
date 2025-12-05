c5fe82402096d72e9cd70f74931b9283fd2fe09d
dhruvsachani99-ui <dhruvsachani99@gmail.com>
2025-12-05 18:13:14 +0530

    Create variable.py

---
+++ 02_Day_Variables_builtin_functions/variable.py
@@ -0,0 +1,25 @@
+#Day 2
+first_name = "DHRUV"
+last_name = "Sachani"
+full_name = first_name + " " + last_name
+country = "India"
+city = "Surat"
+age = 20
+year = 2006
+is_student = True
+is_true = True
+is_light_on = False
+
+a, b, c = 1, 2, 3
+
+print("first_name: ", first_name)
+print("last_name: ", last_name)
+print("full_name: ", full_name)
+print("country: ", country)
+print("city: ", city)
+print("age: ", age)
+print("year: ", year)
+print("student: ", is_student)
+print("Is True: ", is_true)
+print("Light On: ", is_light_on)
+print("variable: ", a, b, c)
